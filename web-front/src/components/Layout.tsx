bd3b9b162db934fdd5d3e0ee3cfcef0b9432a84f
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-06-27 10:27:33 +0900

    e2eテスト作成

--- web-front/src/components/Layout.tsx
+++ web-front/src/components/Layout.tsx
@@ -1,7 +1,14 @@
-import React, { FC, ReactNode } from "react";
+import { signInWithCustomToken } from "firebase/auth";
+import { useRouter } from "next/router";
+import React, { FC, ReactNode, useEffect } from "react";
+import { auth } from "../utils/firebase/firebase";
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
-  
+  const router = useRouter();
+  const customToken = router.query.customToken as string | undefined;
+  useEffect(() => {
+    if (customToken) signInWithCustomToken(auth, customToken);
+  }, [router.isReady]);
   return <>{children}</>;
 };
 
