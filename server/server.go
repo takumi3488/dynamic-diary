8f663c788ed7013aa5f049c7437c92a76fae2e7c
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-07-04 21:19:00 +0900

    graphiql削除

--- server/server.go
+++ server/server.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi"
 	"github.com/rs/cors"
 	"github.com/takumi3488/dynamic-diary/server/auth"
@@ -36,10 +35,8 @@ func main() {
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	router.Handle("/query", srv)
 	log.Printf("Allowed origins are: %s", allowed_origins)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
